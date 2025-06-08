d527e8c326e18319f3e64f6cd119efa27e1e8024
麻婆大豆腐。 <wangshuai9595@163.com>
2025-06-08 19:20:07 +0800

    ListView学习

---
+++ Source/FrontEnd/FrontEnd.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class FrontEnd : ModuleRules
+{
+    public FrontEnd(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core", "UMG",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/FrontEnd/Private/FrontEnd.cpp
@@ -0,0 +1,17 @@
+﻿#include "FrontEnd.h"
+
+#define LOCTEXT_NAMESPACE "FFrontEndModule"
+
+void FFrontEndModule::StartupModule()
+{
+    
+}
+
+void FFrontEndModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FFrontEndModule, FrontEnd)
